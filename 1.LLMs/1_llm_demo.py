23207d32324c1171f5b570cfd20723b5b39042dd
Manthan Jadav <144726258+Manthan2110@users.noreply.github.com>
2026-01-02 21:43:51 +0530

    Update Google API key placeholder in demo script

--- 1.LLMs/1_llm_demo.py
+++ 1.LLMs/1_llm_demo.py
@@ -2,10 +2,9 @@ from langchain_google_genai import GoogleGenerativeAI
 
 llm = GoogleGenerativeAI(
     model='gemini-2.5-flash',
-    google_api_key="AIzaSyAyYnvE6x-wvQFaa7MSCv6wgqFQCks6jos"
+    google_api_key="YOUR_GOOGLE_API_KEY"
 )
 
 result = llm.invoke("Who is the Prime Minister of India now")
 
 print(result)
-
