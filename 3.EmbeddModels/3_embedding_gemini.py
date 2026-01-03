1d2a264d01d8518f75a130b18defce1e6933c9e2
Manthan Jadav <144726258+Manthan2110@users.noreply.github.com>
2026-01-03 23:41:50 +0530

    Update API key to use a placeholder
    Replaced the Google API key with a placeholder.

--- 3.EmbeddModels/3_embedding_gemini.py
+++ 3.EmbeddModels/3_embedding_gemini.py
@@ -2,8 +2,9 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 embedding = GoogleGenerativeAIEmbeddings(
     model="models/embedding-001",
-    google_api_key="AIzaSyBNog1XNZTQDBctMWDc9cKNENA-tUfd0Bc"
+    google_api_key="YOUR_GOOGLE_API_KEY"
 )
 
 vector = embedding.embed_query("Gandhinagar is capital of Gujarat.")
 print(str(vector))
+
